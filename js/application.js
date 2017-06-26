fe619da2396601dcf30210fb5025f044f6059c62
Alex Gladd <alex@alexgladd.com>
2017-06-25 22:26:59 -0400

    Working search result retrieval and population

--- js/application.js
+++ js/application.js
@@ -6,7 +6,8 @@
 
 // globals ------------------------------------------------
 
-var wikiApiUri = "https://en.wikipedia.org/w/api.php?origin=*&action=query&list=search&format=json&utf8=&srlimit=9&srsearch="
+var wikiApiUri = "https://en.wikipedia.org/w/api.php?origin=*&action=query&list=search&format=json&utf8=&srlimit=9&srsearch=";
+var wikiLinkUri = "https://en.wikipedia.org/wiki/";
 
 // jQuery start -------------------------------------------
 
@@ -33,6 +34,10 @@ function onSearch() {
   var searchTxt = $("#wsearch-input").val().trim();
 
   if (searchTxt) {
+    // clear existing results
+    $("#wsearch-results > .row").html("");
+
+    // fire api request
     var uri = encodeURI(wikiApiUri + searchTxt);
     console.log(uri);
     $.getJSON(uri, onSearchResults);
@@ -41,4 +46,35 @@ function onSearch() {
 
 function onSearchResults(results) {
   console.log(results);
+
+  for (var i = 0; i < results.query.search.length; i++) {
+    var sresult = results.query.search[i];
+    console.log(sresult);
+
+    $("#wsearch-results > .row").append(createSearchResult(sresult.title, sresult.snippet, i));
+  }
+}
+
+function createSearchResult(title, snippet, index) {
+  var $result = $("<div></div>").addClass("col-xs-12 col-sm-6 col-md-4");
+  var $link = $("<a></a>")
+    .attr("href", encodeURI(wikiLinkUri + title))
+    .attr("target", "_blank")
+    .addClass("wsearch-result");
+
+  var cardClass = (index % 2 == 0) ? "wsearch-result-a" : "wsearch-result-b";
+  var $resultCard = $("<div></div>").addClass(cardClass);
+  $resultCard.append(
+    $("<h3></h3>").html(title)
+  );
+  $resultCard.append(
+    $("<p></p>").html(snippet)
+  );
+
+  $link.append($resultCard);
+  $result.append($link);
+
+  console.log($result);
+
+  return $result;
 }
